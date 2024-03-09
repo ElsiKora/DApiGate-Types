284bc26088a665bd5e81309835b8283a55b5885f
Keenest Callas <admin@elsikora.com>
2024-03-09 18:06:45 +0300

    gitRefactor rate types and update version
    - Refactor rate module by centralizing interface definitions - Introduce new `IRate` and
    `IRateGetListProperties` interfaces for improved type consistency - Increment package version to `1.0.7`
    to prepare for new release
    
    empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # #
    Changes to be committed: # modified:  package.json # new file:
    rate/interface/get-list-properties.interface.ts # new file:  rate/interface/interface.ts # modified:
    rate/rate.d.ts #

---
+++ rate/interface/interface.ts
@@ -0,0 +1,6 @@
+// Version: 1.0.0
+
+export interface IRate {
+	currency: string;
+	rate: number;
+}
--- rate/rate.d.ts
+++ rate/rate.d.ts
@@ -1,7 +1,6 @@
 // Version: 1.0.0
 
 export declare namespace Rate {
-	export interface IRateGetListProperties {
-		base: string;
-	}
+	export type { IRateGetListProperties } from "./interface/get-list-properties.interface";
+	export type { IRate } from "./interface/interface";
 }
